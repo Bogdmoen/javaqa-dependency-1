3daf329c68c54e50a47fa5d6df04725a9551b351
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-02-13 06:41:50 +0300

    repository tests added

--- src/test/java/ru/netology/repository/FilmsRepositoryTest.java
+++ src/test/java/ru/netology/repository/FilmsRepositoryTest.java
@@ -7,30 +7,59 @@ import ru.netology.domain.FilmUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmsRepositoryTest {
+    FilmsRepository repository = new FilmsRepository();
+    FilmUnit first = new FilmUnit(1, 1, "first", 44, "url", "date");
+    FilmUnit second = new FilmUnit(2, 2, "first", 12, "url", "date");
+    FilmUnit third = new FilmUnit(3, 3, "first", 5, "url", "date");
+    FilmUnit forth = new FilmUnit(4, 3, "first", 19, "url", "date");
+    FilmUnit fifth = new FilmUnit(5, 3, "first", 44, "url", "date");
+    FilmUnit sixth = new FilmUnit(6, 3, "first", 44, "url", "date");
 
     @Test
-    void save() {
-        FilmsRepository repository = new FilmsRepository();
-        FilmUnit first = new FilmUnit(1, 1, "first", 44, "url", "date");
-        FilmUnit second = new FilmUnit(2, 2, "first", 12, "url", "date");
-        FilmUnit third = new FilmUnit(3, 3, "first", 5, "url", "date");
-        FilmUnit forth = new FilmUnit(4, 3, "first", 19, "url", "date");
-        FilmUnit fifth = new FilmUnit(5, 3, "first", 44, "url", "date");
-        FilmUnit sixth = new FilmUnit(6, 3, "first", 44, "url", "date");
+    void shouldSave() {
 
         repository.save(first);
         repository.save(second);
+        repository.save(third);
 
-        FilmUnit[] expected = repository.findAll();
-        FilmUnit[]
+        FilmUnit[] actual = repository.findAll();
+        FilmUnit[] expected = new FilmUnit[] {first, second, third};
+
+        assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void shouldRemoveById() {
+
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        repository.removeById(2);
+
+        FilmUnit[] actual = repository.findAll();
+        FilmUnit[] expected = new FilmUnit[] {first, third};
+
+        assertArrayEquals(expected, actual);
 
     }
 
     @Test
-    void findAll() {
+    void shouldNotRemoveNonExistedId() {
+
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        repository.removeById(4);
+
+        FilmUnit[] actual = repository.findAll();
+        FilmUnit[] expected = new FilmUnit[] {first, second, third};
+
+        assertArrayEquals(expected, actual);
+
     }
 
-    @Test
-    void removeById() {
-    }
 }
