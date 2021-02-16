a666f2ca2872b4c8dc2d6ae37a51a41ad6037ed1
Bogdan Klimchuk <bogd.moen@yandex.com>
2021-02-16 04:16:34 +0300

    tests fixed

--- src/test/java/ru/netology/manager/FilmFeedManagerCustomTest.java
+++ src/test/java/ru/netology/manager/FilmFeedManagerCustomTest.java
@@ -21,21 +21,25 @@ public class FilmFeedManagerCustomTest {
     private FilmFeedManager manager = new FilmFeedManager(5, 44);
 
     FilmUnit first = new FilmUnit(1, 1, "first", 44, "url", "date");
-    FilmUnit second = new FilmUnit(2, 2, "first", 44, "url", "date");
-    FilmUnit third = new FilmUnit(3, 3, "first", 44, "url", "date");
-    FilmUnit forth = new FilmUnit(4, 3, "first", 44, "url", "date");
+    FilmUnit second = new FilmUnit(2, 2, "first", 55, "url", "date");
+    FilmUnit third = new FilmUnit(3, 3, "first", 49, "url", "date");
+    FilmUnit forth = new FilmUnit(4, 3, "first", 19, "url", "date");
     FilmUnit fifth = new FilmUnit(5, 3, "first", 44, "url", "date");
     FilmUnit sixth = new FilmUnit(6, 3, "first", 44, "url", "date");
+    FilmUnit seventh = new FilmUnit(7, 3, "first", 44, "url", "date");
+    FilmUnit eights = new FilmUnit(8, 3, "first", 44, "url", "date");
+    FilmUnit ninth = new FilmUnit(9, 3, "first", 44, "url", "date");
+    FilmUnit tenth = new FilmUnit(10, 3, "first", 44, "url", "date");
 
     @Test
    public void shouldGetFilmListWithCustomLimit() {
 
 
-        FilmUnit[] returned = new FilmUnit[] {first, second, third, forth, fifth, sixth};
+        FilmUnit[] returned = new FilmUnit[] {first, fifth, sixth, seventh, eights, ninth, tenth};
         doReturn(returned).when(repository).findAll();
 
         FilmUnit[] expected = manager.getFilmList();
-        FilmUnit[] actual = new FilmUnit[]{sixth, fifth, forth, third, second};
+        FilmUnit[] actual = new FilmUnit[]{tenth, ninth, eights, seventh, sixth};
 
         assertArrayEquals(expected, actual);
     }
@@ -52,7 +56,4 @@ public class FilmFeedManagerCustomTest {
         assertArrayEquals(expected, actual);
 
     }
-
-
-
 }
